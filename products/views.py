9f9accfbc466b444a7ec971f991f15572f4a616d
katladva <kat.ladva@hotmail.com>
2023-04-16 14:01:43 +0300

    first commit

---
+++ products/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(requests):
+    context = {
+        'product_entries': [
+            {
+                'title': 'Roses',
+                'body': 'New breed with excellent color!',
+            },
+            {
+                'title': 'Roses',
+                'body': 'Magic black color!',
+            },
+            {
+                'title': 'Daisy',
+                'body': 'Rustic beauty!',
+            }
+        ]
+    }
+    return render(requests, "home.html", context)
+
+def products(requests):
+    return render(requests, "products.html")
